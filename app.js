6d7492a9a92ca60ba54f36292fd5254e7923bdb9
aigik <aigi.kallaste@khk.ee>
2019-11-14 20:56:35 +0200

    document objekt - omadused

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
-// window objects / methods / properties
+// document
 
-// alert
- // window.alert('Tere Hommikust!');
- // alert('Tere Hommikust!');
+let sisu;
 
- // propt
- // const sisend = prompt('Sisesta oma nimi');
- // alert('Tere Hommikust, ' + sisend + '!');
+sisu = document; // sisu html-is
+sisu = document.all; // html tagid kasutamisj2rjekorras
+sisu = document.all[2]; // konkreetne html tag
+sisu = document.all.length; // palju on html tagi kasutusel
 
- // confirm
- // if(confirm('Kas oled n6us?')){
- //    console.log('jah');
- //} else {
-//   console.log('ei');
- //}
-
- // ekraani v2lim66dud pikslites
- // let valiKorgus = window.outerHeight;
- // let valiLaius = window.outerWidth;
- // console.log(valiLaius)
-
- // ekraani sisem66dud (content area) piksites
- // let siseKorgus = window.innerHeight;
- // let siseLaius = window.innerWidth;
- // console.log(siseLaius)
-
- //scroll (pikslite arv scrollides)
- // scrolY = window.scrollY;
- // scrolX = window.scrolX;
- // console.log(scrolY)
-
- // location objekt (to get current page address (url)and to redirect the browser to a new page)
- let objekt = window.location;
- let hostname = window.location.hostname; // returns the domain name of the web host
- let port = window.location.port; // returns the number of the internet host port (of the current page)
- let href = window.location.href; // returns the href (URL) of the current page
- let search = window.location.search;
-
- // redirect
- // window.location.href = 'https://khk.ee';
- // reload
- // window.location.reload();
-
- console.log(search);
+// document properties - objekti omadused
+sisu = document.head; // head sisu
+sisu = document.body; // body sisu
+sisu = document.doctype; // doctype
+sisu = document.domain;
+sisu = document.URL;
+sisu = document.characterSet;
+sisu = document.contentType;
+console.log(sisu);
