595ccd1cfecb9e23ccaba67e0dd08e5c2426922d
aigik <aigi.kallaste@khk.ee>
2019-11-14 19:28:31 +0200

    window omadused

--- app.js
+++ app.js
@@ -5,12 +5,27 @@
  // alert('Tere Hommikust!');
 
  // propt
- const sisend = prompt('Sisesta oma nimi');
- alert('Tere Hommikust, ' + sisend + '!');
+ // const sisend = prompt('Sisesta oma nimi');
+ // alert('Tere Hommikust, ' + sisend + '!');
 
  // confirm
- if(confirm('Kas oled n6us?')){
-     console.log('jah');
- } else {
-     console.log('ei');
- }
+ // if(confirm('Kas oled n6us?')){
+ //    console.log('jah');
+ //} else {
+//   console.log('ei');
+ //}
+
+ // ekraani v2lim66dud pikslites
+ let valiKorgus = window.outerHeight;
+ let valiLaius = window.outerWidth;
+ // console.log(valiLaius)
+
+ // ekraani sisem66dud (content area) piksites
+ let siseKorgus = window.innerHeight;
+ let siseLaius = window.innerWidth;
+ // console.log(siseLaius)
+
+ //scroll (pikslite arv scrollides)
+ scrolY = window.scrollY;
+ scrolX = window.scrolX;
+ console.log(scrolY)
