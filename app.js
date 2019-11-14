d1cb50aa95bfd61792a7643d5b7b0dfd718d4baa
aigik <aigi.kallaste@khk.ee>
2019-11-14 21:45:36 +0200

    document.querySelector() stiili muutmine

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 // document meetodid
 // single element selectors
 
-sisu = document.getElementById('title'); // html sisu
-id = document.getElementById('title').id; // id
-klass = document.getElementById('title').className;
+sisu = document.querySelector('#title'); // id j2rgi (get the first element with id title)
+sisu = document.querySelector('.card-title'); // klassi nime j2rgi (get the first element with class card-title)
+sisu = document.querySelector('h5'); // elemendi j2rgi
 
 // stiili muutmine
-// sisu.style.background = '#333';
- document.getElementById('title').style.background = '#333';
- sisu.style.color = '#fff';
- sisu.style.padding = '5px';
-// sisu.style.display = 'none';
-
-// sisu muutmine
-sisu.textContent = 'Minu ylesanded';
-sisu.innerText = 'Just minu ylesanded';
-sisu.innerHTML = '<span style="color:lightgreen">Vabad ylesanded</span>';
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(2)').style.color = 'orange';
 console.log(sisu);
