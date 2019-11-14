2011d24693a16b9fdb6b9525496d4567a977ac6a
aigik <aigi.kallaste@khk.ee>
2019-11-14 19:51:25 +0200

    window objekt - location

--- app.js
+++ app.js
@@ -16,16 +16,30 @@
  //}
 
  // ekraani v2lim66dud pikslites
- let valiKorgus = window.outerHeight;
- let valiLaius = window.outerWidth;
+ // let valiKorgus = window.outerHeight;
+ // let valiLaius = window.outerWidth;
  // console.log(valiLaius)
 
  // ekraani sisem66dud (content area) piksites
- let siseKorgus = window.innerHeight;
- let siseLaius = window.innerWidth;
+ // let siseKorgus = window.innerHeight;
+ // let siseLaius = window.innerWidth;
  // console.log(siseLaius)
 
  //scroll (pikslite arv scrollides)
- scrolY = window.scrollY;
- scrolX = window.scrolX;
- console.log(scrolY)
+ // scrolY = window.scrollY;
+ // scrolX = window.scrolX;
+ // console.log(scrolY)
+
+ // location objekt (to get current page address (url)and to redirect the browser to a new page)
+ let objekt = window.location;
+ let hostname = window.location.hostname; // returns the domain name of the web host
+ let port = window.location.port; // returns the number of the internet host port (of the current page)
+ let href = window.location.href; // returns the href (URL) of the current page
+ let search = window.location.search;
+
+ // redirect
+ // window.location.href = 'https://khk.ee';
+ // reload
+ // window.location.reload();
+
+ console.log(search);
