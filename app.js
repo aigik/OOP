09be4661353bdc90312d3ddd721ae1de97fb62b7
aigik <aigi.kallaste@khk.ee>
2019-11-15 13:45:50 +0200

    node omadused ja meetodid

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
 // document meetodid
-// multiply elements selectors
-let sisu = document.getElementsByClassName('collection-item');
+// node omadused ja meetodid
+
+let nimekiri = document.querySelectorAll('ul.collection');
+let nimekirjaElement = document.querySelectorAll('ul.collection-item');
+
+console.log(nimekiri);
+console.log(nimekirjaElement);
+
+nimekiri = document.querySelector('ul.collection');
+console.log(nimekiri);
+
+sisu = nimekiri.childNodes;
+sisu = nimekiri.childNodes[0];
+sisu = nimekiri.childNodes[0].nodeName;
+sisu = nimekiri.childNodes[3].nodeType;
+
+/*
+erinevad node tüübid
+1 - element
+2 - atribuut
+3 - tekst
+8 - kommentaar
+9 - dokument ise
+10 - doctype
+*/
+sisu = nimekiri.children;
+sisu = nimekiri.children[1];
+nimekiri.children[1].textContent = 'Õpi rohkem JS';
+sisu = nimekiri.children[2].children[0].id = 'katse';
+
 console.log(sisu);
-console.log(sisu[2]);
-sisu[0].style.color = 'orange';
-sisu[2].textContent = 'Õpi arvutivõrgud';
+console.log(nimekiri.children[2].children[0]);
 
-// querySelector + klassi nime järgi
-let nimekirjaElemendid = document.querySelector('ul').getElementsByClassName('collection-item');
+sisu = nimekiri.firstChild; // node type
+sisu = nimekiri.firstElementChild; // value
+console.log(sisu);
 
-// elemendi nime järgi
-nimekirjaElemendid = document.getElementsByTagName('li');
+sisu = nimekiri.lastChild; // node type
+sisu = nimekiri.lastElementChild; // value
 
-// teisendame HTMLCollection massiiviks (array)
-nimekirjaElemendid = Array.from(nimekirjaElemendid);
-// massiivi meetodite kasutamine
-nimekirjaElemendid.reverse();
+sisu = nimekiri.childElementCount;
 
-// massiivi läbimine tsükliga
-// for
-for(let i = 0; i < nimekirjaElemendid.length; i++){
-    nimekirjaElemendid[i].style.background = '#ddd';
-    nimekirjaElemendid[1].style.background = '#f4f4f4';
-//console.log(nimekirjaElemendid[i].className);
-}
+nimekirjaElement = document.querySelector('li.collection-item:nth-child(2)');
+sisu = nimekirjaElement.parentNode;
+sisu = nimekirjaElement.parentElement;
+sisu = nimekirjaElement.parentElement.parentElement;
 
-// forEach
-//nimekirjaElemendid.forEach(function(element){
-  //  console.log(element);
-    //element.style.background = '#ddd';
-//});
+sisu = nimekirjaElement.nextSibling; // node type
+sisu = nimekirjaElement.nextElementSibling; // value
 
-let nimekiriPaaritu = document.querySelectorAll('li:nth-child(odd)');
-console.log(nimekiriPaaritu);
-
-let nimekiriPaaris = document.querySelectorAll('li:nth-child(even)');
-console.log(nimekiriPaaris);
+sisu = nimekirjaElement.previousSibling; // node type
+sisu = nimekirjaElement.previousElementSibling; // value
+console.log(sisu);
