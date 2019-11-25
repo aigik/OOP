ad8150b682d786e723392dafe8360d72d14919d4
aigik <aigi.kallaste@khk.ee>
2019-11-25 11:50:44 +0200

    inimene kirjeldus - objekt.prototype

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 // inimene kirjeldus OOP abil
+// objekt.prototype
 // inimese konstruktor (skp=synnikuupäev)
 function Isik(e, p, skp){
     this.eesnimi = e;
     this.perenimi = p;
     this.synnikuupaev = new Date(skp);
-
+}
     // väljasta täisnimi
-    this.taisNimi = function(){
+    Isik.prototype.taisNimi = function(){
         return `${this.eesnimi} ${this.perenimi}`;
     }
-
+    
     // arvuta vanus
-    this.arvutaVanus = function(){
+    Isik.prototype.arvutaVanus = function(){
         const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
         const vanusDateKujul = new Date(vaheSekundites);
         const taisAasta = vanusDateKujul.getUTCFullYear();
         const vanus = taisAasta - 1970;
         return vanus;
-    }
 }
+
+/* isiku prototüübile on lisatud funktsionaalsus
+Isik.prototype.taisNimi = function(){
+    return `${this.eesnimi} ${this.perenimi}`;
+}*/
 
 const aigi = new Isik('Aigi', 'Kallaste', '1983-08-13');
 const kadi = new Isik('Kadi', 'Tamm', '1989-12-23');
-console.log(aigi.arvutaVanus());
-console.log(kadi.arvutaVanus());
+console.log(aigi);
+console.log(kadi);
