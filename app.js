09a2c202fc55dbbc2a96f2fa3c2cebe234b64779
aigik <aigi.kallaste@khk.ee>
2019-11-25 15:46:43 +0200

    raamatu kontruktor - teate väljastamine

--- app.js
+++ app.js
@@ -19,12 +19,43 @@ KL.prototype.puhastaSisend = function(){
     document.getElementById('isbn').value = '';
 }
 
+// raamatu lisamine tabelisse
+KL.prototype.lisaRaamatTabelisse = function(r){
+    // loome tabeli rida
+    const rida = document.createElement('tr');
+    // täidame rida tabeli andmetega
+    rida.innerHTML = `
+    <td>${r.autor}</td>
+    <td>${r.pealkiri}</td>
+    <td>${r.isbn}</td>
+    `;
+    // lisame rida tabelisse
+    tabel = document.getElementById('book-list');
+    tabel.appendChild(rida);
+}
+
+// teate väljastamine
+KL.prototype.teade = function(s){
+    // loome div, kuhu lisada teate sõnum
+    const div = document.createElement('div');
+    // lisame sõnumi teksti div sisse
+    const tekst = document.createTextNode(s);
+    div.appendChild(tekst);
+    // leiame elemendid, mille suhtes tuleb lisada uus element
+    const konteiner = document.querySelector('.container');
+    console.log(konteiner);
+    const vorm = document.getElementById('book.fomr');
+    // lisame teade dokumendi
+    konteiner.insertBefore(div, vorm);
+    
+}
+
 // kirjeldame raamatu lisamise sündmust
 document.getElementById('book-form') .addEventListener('submit', lisaRaamat);
 // raamatu lisamise funktsioon
 function lisaRaamat(e){
-    const pealkiri = document.getElementById('author').value;
-    const autor = document.getElementById('title').value;
+    const pealkiri = document.getElementById('title').value;
+    const autor = document.getElementById('author').value;
     const isbn = document.getElementById('isbn').value;
 
 // loome raamat andmete põhjal
@@ -34,6 +65,19 @@ const raamat = new Raamat(pealkiri, autor, isbn);
 
     // loome kasutajaliidese objekt temaga opereerimiseks
     const kl = new KL();
+
+    // kui mingid andmed on puudu, siis anname märku
+    if(pealkiri == '' | autor == '' | isbn == ''){
+        kl.teade('Tuleb täita kõik väljad!');
+    } else {
+        // muidu
+        // lisame sisestatud raamat tabelisse
+        kl.lisaRaamatTabelisse(raamat);
+        kl.teade('Raamat on lisatud!');
+    }
+
+    // lisame sisestatud raamat tabelisse
+    kl.lisaRaamatTabelisse(raamat);
 
     // puhastame väljad sisestatud andmetest
     kl.puhastaSisend();
