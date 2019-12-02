06f63ad5a78c28f6dffa3db34e515b2cc2e68231
aigik <aigi.kallaste@khk.ee>
2019-12-02 10:39:38 +0200

    alert kustumine 5s pärast - setTimeout

--- app.js
+++ app.js
@@ -38,15 +38,21 @@ KL.prototype.lisaRaamatTabelisse = function(r){
 KL.prototype.teade = function(s){
     // loome div, kuhu lisada teate sõnum
     const div = document.createElement('div');
+    div.className = 'alert';
     // lisame sõnumi teksti div sisse
     const tekst = document.createTextNode(s);
     div.appendChild(tekst);
     // leiame elemendid, mille suhtes tuleb lisada uus element
     const konteiner = document.querySelector('.container');
     console.log(konteiner);
-    const vorm = document.getElementById('book.fomr');
+    const vorm = document.getElementById('book.form');
     // lisame teade dokumendi
     konteiner.insertBefore(div, vorm);
+
+    // kutsume teade 5 sekundi möödumisel
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 5000);
     
 }
 
@@ -81,6 +87,9 @@ const raamat = new Raamat(pealkiri, autor, isbn);
 
     // puhastame väljad sisestatud andmetest
     kl.puhastaSisend();
+
+   
+    
 
     e.preventDefault();
 }
