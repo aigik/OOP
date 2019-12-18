7acecd2238bb45368b53254afe6b052053bb73e3
aigik <aigi.kallaste@khk.ee>
2019-12-18 11:30:54 +0200

    JSON andmete lugemine + failide parandused

---
+++ 18.12 Data Fetch/app.js
@@ -0,0 +1,40 @@
+document.getElementById('btn1').addEventListener('click', getTextData);
+document.getElementById('btn2').addEventListener('click', getJsonData);
+
+// local json file
+function getJsonData() {
+    fetch('data.json')
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let out = '';
+        out = '';
+        data.forEach(function(timetable){
+            out = out + `<li>${timetable.opetaja}`;
+            out = out + `- ${timetable.aine}</li>`;
+            console.log(out);
+        });
+        document.getElementById('out').innerHTML = out;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+// local text file
+function getTextData() {
+    fetch('text.txt')
+    .then(function(res){
+        return res.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('out')
+        .innerHTML = data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
